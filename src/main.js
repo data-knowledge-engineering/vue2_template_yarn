0c218d1f925f17745c33d454a2804b126575d520
李昊霖 <sdk1373@gmail.com>
2022-03-01 16:35:57 +0800

    增加ElementUI，Echarts模块

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// 全局引入ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   router,
   store,
+  el: '#app',
   render: h => h(App)
-}).$mount('#app')
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button>这是一个el按钮</el-button>
+    <div id="chart1"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import * as echarts from 'echarts';
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  created() {
+    
+  },
+  mounted(){
+    this.initChart();
+  },
+  methods: {
+    initChart() {
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById("chart1"));
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: "ECharts 入门示例",
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      });
+    },
+  },
+};
 </script>
+<style>
+  #chart1{
+    width: 300px;
+    height: 300px;
+  }
+</style>
